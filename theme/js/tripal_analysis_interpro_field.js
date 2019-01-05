0b9e43a1d4a7a11b354a2f3377ee0f50852cbd54
bradfordcondon <bradford.condon@gmail.com>
2019-01-04 20:07:51 -0500

    convert php to js

--- theme/js/tripal_analysis_interpro_field.js
+++ theme/js/tripal_analysis_interpro_field.js
@@ -1,15 +1,31 @@
-(function($) {
-    // hack to work with multiple jquery versions
-    backup_jquery = window.jQuery;
-    window.jQuery = $;
-    window.$ = $;
-    window.ft".$analysis->analysis_id.".addFeature({
-        data: ".json_encode($hsp_pos).",
-        name: "".$viz_name."",
-    className: "ipr_match", //can be used for styling
-    color: "#0F8292",
-    type: "rect"
-});
-    window.jQuery = backup_jquery;
-    window.$ = backup_jquery;
-})(feature_viewer_jquery);
+(function ($) {
+
+    Drupal.behaviors.tripal_analysis_blast = {
+        attach: function (context, settings) {
+
+            var instance = settings.tripal_analysis_interpro;
+            var analysis_id = instance.analysis_id
+            var data = instance.data
+            var name = instance.name
+
+            $("#tripal-interpro-analysis-accordian").accordion({
+                collapsible: true,
+                active: false,
+                heightStyle: "content"
+            });
+            // hack to work with multiple jquery versions
+            backup_jquery = window.jQuery;
+            window.jQuery = $;
+            window.$ = $;
+            window.ft[analysis_id].addFeature({
+                data: data,
+                name: name,
+                className: "ipr_match",
+                color: "#0F8292",
+                type: "rect"
+            });
+            window.jQuery = backup_jquery;
+            window.$ = backup_jquery;
+        }
+    }
+})(jQuery);
