5aacefbc44238b888022e97701347fc6b3e48180
bradfordcondon <bradford.condon@gmail.com>
2019-01-07 10:31:08 -0500

    WIP

--- theme/js/tripal_analysis_interpro_field.js
+++ theme/js/tripal_analysis_interpro_field.js
@@ -1,31 +1,68 @@
 (function ($) {
 
-    Drupal.behaviors.tripal_analysis_blast = {
+    Drupal.behaviors.tripal_analysis_interpro = {
         attach: function (context, settings) {
-
-            var instance = settings.tripal_analysis_interpro;
-            var analysis_id = instance.analysis_id
-            var data = instance.data
-            var name = instance.name
 
             $("#tripal-interpro-analysis-accordian").accordion({
                 collapsible: true,
                 active: false,
                 heightStyle: "content"
             });
-            // hack to work with multiple jquery versions
-            backup_jquery = window.jQuery;
-            window.jQuery = $;
-            window.$ = $;
-            window.ft[analysis_id].addFeature({
-                data: data,
-                name: name,
-                className: "ipr_match",
-                color: "#0F8292",
-                type: "rect"
+
+
+            // For each element of class "interpro-alignment-viewer" add the
+            // feature viewer.
+
+            $(".interpro-alignment-viewer").each(function() {
+
+                $(this).css('width', $(this).attr("width"));
+                // Create the feature viewer for this analysis.
+                var id = $(this).attr('id');
+
+                var analysis_id = $(this).attr('analysis_id');
+                var residues = $(this).attr('residues');
+                var options = {
+                    showAxis: true,
+                    showSequence: true,
+                    brushActive: true,
+                    toolbar: true,
+                    bubbleHelp: true,
+                    zoomMax: 3
+                }
+                var fv = new FeatureViewer(residues, '#' + id, options);
+
+
+                // Add info from data table.
+
+               $(".tripal-analysis-ipr-fv-" + analysis_id).each(function() {
+
+                    var hit_name = $(this).attr('name');
+                 //   var class_name = $(this).attr('fv_class');
+                    var data = JSON.parse($(this).attr('fv_data'));
+                    console.log(data)
+                   console.log("hi")
+                    if (data) {
+                        fv.addFeature({
+                            data: data,
+                            name: hit_name,
+                        //    className: class_name,
+                            color: "#888888",
+                            type: "rect"
+                        });
+                    }
+                })
+
+                fv.onFeatureSelected(function (d) {
+                    var id = d.detail.id;
+                    var re = /tripal-ipr-domain-(\d+)-(\d+)/;
+                    var matches = id.match(re);
+                    var analysis_id = matches[1];
+                    var j = matches[2];
+                    // $(".tripal-analysis-blast-info-hsp-desc").hide();
+                    // $("#hsp-desc-" + analysis_id + "-" +j).show();
+                });
             });
-            window.jQuery = backup_jquery;
-            window.$ = backup_jquery;
+
         }
     }
 })(jQuery);
